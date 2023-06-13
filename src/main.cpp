dbde3c0c623de3cae354f3cdfdf396c055ed90f8
Shinde <petrenko200903@gmail.com>
2023-06-13 22:16:58 +0700

    Example main file update

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,28 @@
 
 int main()
 {
-  Queue<int> q;
-  printf("deleted: %d\n", q.pop());
-  q.push(1);
-  q.push(2);
-  q.push(3);
-  q.clear();
-  printf("deleted: %d\n", q.pop());
-  q.push(4);
-  printf("deleted: %d\n", q.pop());
-  q.display();
+  Queue<int> q; // Создаем очередь целых чисел
 
-  int a = 1, b = 2;
-  Queue<int *> q2;
-  printf("deleted: %p\n", q2.pop());
-  q2.push(&a);
-  q2.push(&b);
-  q2.display();
+  q.push(1);   // Добавление элемента
+  q.push(2);   // Добавление элемента
+  q.push(3);   // Добавление элемента
+  q.display(); // Смотрим очередь
+  q.pop();     // Достаем элемент
+  q.display(); // Смотрим очередь
+  q.clear();   // Очищаем очередь
+  q.display(); // Смотрим очередь
+  q.push(4);   // Добавляем элемент
+  q.display(); // Смотрим очередь
+
+  string a = "aaa", b = "bbb", c = "ccc";
+  Queue<string> q2; // Создаем очередь строк
+
+  q2.push(a);   // Добавление элемента
+  q2.push(b);   // Добавление элемента
+  q2.display(); // Смотрим очередь
+  q2.push(c);   // Добавление элемента
+  q2.pop();     // Достаем элемент
+  q2.display(); // Смотрим очередь
 
   return 0;
 }
